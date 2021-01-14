9ae90e0482b741dedd4dbb3aea2432e81e8bc0fb
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-14 13:32:32 -0500

    Added postal code regular expression validation

--- com/luv2code/springdemo/mvc/Customer.java
+++ com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -18,6 +19,9 @@ public class Customer {
 	@Min(value=0, message="Must be greater than or equal to 0")
 	@Max(value=10, message="Must be less than or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="Only 5 characters/digits allowed")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -41,6 +45,14 @@ public class Customer {
 
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 	
